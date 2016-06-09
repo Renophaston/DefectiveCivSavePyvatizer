1eadaa0dc3c43ab52e1c6fe9bff81de274bbb706
Kristopher Lehman-Brown <renophaston@gmail.com>
2016-06-08 18:05:27 -0700

    Currently it just copies a file

--- dcivsavepyvatizer.py
+++ dcivsavepyvatizer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
-from os.path import basename
+from os.path import splitext
 
 ### "config" kind of...?
 # the extension of Civ5 save files
@@ -14,12 +14,23 @@ parser = argparse.ArgumentParser()
 parser.add_argument('inputfile', help='input save file')
 parser.add_argument('--outputfile', '-o', help='output save file')
 
+# parse the command line arguments
 args = parser.parse_args()
 
+# the name of the input file
+inputfile = args.inputfile
+
+# use the output filename as given if given on the command line
 if args.outputfile:
     outputfile = args.outputfile
 else:
-    outputfile = basename(args.inputfile) + '_priv.' + extension
+    # strip the file extension (final part) and add the extension
+    outputfile = splitext(args.inputfile)[0] + '_priv.' + extension
 
 
-print(outputfile)
+with open(inputfile, 'rb') as f:
+    # read the entire save file into memory
+    data = f.read()
+with open(outputfile, 'wb') as f:
+    # write the data out to a new file
+    f.write(data)
