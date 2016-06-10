a7ad376ae01d9c3886563a5f94281398eaa3e14b
Kristopher Lehman-Brown <renophaston@gmail.com>
2016-06-09 17:08:04 -0700

    Replacement is made

--- dcivsavepyvatizer.py
+++ dcivsavepyvatizer.py
@@ -2,10 +2,21 @@
 
 import argparse
 from os.path import splitext
+from sys import exit
 
 ### "config" kind of...?
 # the extension of Civ5 save files
 extension = 'Civ5Save'
+
+### the patterns we're looking for
+# We want to find this twice, and replace [7] (0x01) with 0x03
+pattern1 = bytes.fromhex('000000030000000100000040')
+# We want to find this once, then switch the byte three bytes back from this
+# position from 0x00 -> 0x01.
+# NOTE: this seems to be the only one needed sometimes. ??
+pattern2 = bytes.fromhex('ffffffff00000040')
+
+### let us go...!
 
 # for parsing the command line arguments
 parser = argparse.ArgumentParser()
@@ -28,9 +39,29 @@ else:
     outputfile = splitext(args.inputfile)[0] + '_priv.' + extension
 
 
+# read the file into memory
 with open(inputfile, 'rb') as f:
     # read the entire save file into memory
-    data = f.read()
+    data = bytearray(source=f.read())
+
+# check to make sure it matches what we're expecting
+if data.count(pattern2) != 1:
+    # if it doesn't, then quit
+    print("This file doesn't look like what I was expecting.")
+    print("I give up.")
+    exit()
+
+# find the pattern in the data
+pattern2_location = data.find(pattern2)
+
+if pattern2_location < 3:
+    print("Matched pattern way too close to beginning of file...")
+    print("I give up.")
+    exit()
+
+# make the replacement
+data[pattern2_location - 3] = 1
+
 with open(outputfile, 'wb') as f:
     # write the data out to a new file
     f.write(data)
